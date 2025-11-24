574f2dff42cf754cdbab8491e1974edf7d8094a4
Bryce Triplett <brycetriplett@gmail.com>
2025-11-23 18:50:10 -0800

    .

--- nfctest.py
+++ nfctest.py
@@ -1,27 +1,13 @@
 import nfc
 
-
 def on_connect(tag):
     print("Card detected!")
     print("UID:", tag.identifier.hex())
-    return True  # keep connection until card is removed
+    return True
 
+clf = nfc.ContactlessFrontend('usb')  # <-- use 'usb' instead of 'pcsc'
+print("Waiting for card tap...")
 
-def main():
-    try:
-        # Open PC/SC backend
-        clf = nfc.ContactlessFrontend('pcsc')
-        print("Waiting for a card tap...")
+while True:
+    clf.connect(rdwr={'on-connect': on_connect})
 
-        while True:
-            clf.connect(rdwr={'on-connect': on_connect})
-
-    except KeyboardInterrupt:
-        print("\nExiting...")
-    finally:
-        if clf:
-            clf.close()
-
-
-if __name__ == "__main__":
-    main()
